2ce41e0777c0de4186e88209c6333be8676eefee
GabrielAlexandreFerraz <gblferrazrodrigues@gmail.com>
2023-02-25 17:46:21 -0300

    funções de editar e excluir

--- src/componentes/produtos/listagem/index.tsx
+++ src/componentes/produtos/listagem/index.tsx
@@ -1,23 +1,63 @@
 import { Layout, Loader } from "componentes"
 import Link from 'next/link'
+import Router from "next/router"
 import { TabelaProdutos } from './tabela'
 import { Produto } from 'app/models/produtos'
 import useSWR from 'swr'
 import { httpClient } from 'app/http'
 import { AxiosResponse } from "axios"
+import { useProdutoService } from 'app/services'
+import { useState, useEffect } from "react"
+import { Alert } from "componentes/common/message"
 
 export const ListagemProdutos: React.FC = () => {
 
+    const service = useProdutoService();
+    const [ messages, setMessages ] = useState<Array<Alert>>([])
+
     const { data: result, error } = useSWR<AxiosResponse<Produto[]>>('/api/produtos', url => httpClient.get(url))
     
+    const [ lista, setLista] = useState<Produto[]>([])
+
+    useEffect( ()=>{
+        setLista(result?.data || [])
+
+    },[result])
+    
+    const editar = (produto: Produto)=> {
+        const url = `/cadastroPages/produtos?id=${produto.id}`
+        Router.push(url)
+    }
+
+
+    const deletar = (produto: Produto)=> {
+        
+        service.deletar(produto.id).then(response => {
+            
+            setMessages([
+                { tipo: 'success', texto: 'Produto Excluido com Sucesso!' }
+            ])
+            const listaAlterada:Produto[] = lista?.filter(p => p.id != produto.id)
+            setLista(listaAlterada)
+        })
+        .catch(error => {
+            setMessages([
+                { tipo: 'error', texto: 'Não foi possível excluir o produto.' }
+            ])
+        })  
+    }
+
+   
+
     return(
-    <Layout titulo="Produtos">
+    <Layout titulo="Produtos" mensagens={messages}>
         <Link href="/cadastroPages/produtos">
         <button className="button is-warning">Novo </button>
         </Link>
         <br />
+        <br />
         <Loader show={!result}/>
-        <TabelaProdutos produtos={result?.data || []}/>
+        <TabelaProdutos onEdit={editar} onDelete={deletar} produtos={lista}/>
     </Layout>
     )
 
--- src/componentes/produtos/listagem/tabela/index.tsx
+++ src/componentes/produtos/listagem/tabela/index.tsx
@@ -1,11 +1,16 @@
 import { Produto } from 'app/models/produtos'
+import { useState } from 'react';
 
 interface TabelaProdutosProps{
     produtos: Array<Produto>;
+    onEdit: (produto) => void;
+    onDelete: (produto) => void;
 }
 
 export const TabelaProdutos: React.FC<TabelaProdutosProps> = ({
-    produtos
+    produtos,
+    onDelete,
+    onEdit
 }) => {
     return(
         <table className="table is-striped is-hoverable">
@@ -19,7 +24,14 @@ export const TabelaProdutos: React.FC<TabelaProdutosProps> = ({
                 </tr>
             </thead>
             <tbody>
-            {produtos.map(produto => <ProdutoRow key={produto.id} produto={produto}/>)}
+            {produtos.map(produto => (
+                        <ProdutoRow onDelete={onDelete} 
+                                    onEdit={onEdit} 
+                                    key={produto.id} 
+                                    produto={produto}/>
+                            )
+                        )
+                    }
             </tbody>
         </table>
     )
@@ -27,11 +39,29 @@ export const TabelaProdutos: React.FC<TabelaProdutosProps> = ({
 
 interface ProdutoRowProps{
     produto: Produto;
+    onEdit: (produto) => void;
+    onDelete: (produto) => void;
 }
 
 const ProdutoRow: React.FC<ProdutoRowProps> = ({
-    produto
+    produto,
+    onEdit,
+    onDelete
 }) => {
+
+const [deletando, setDeletando] = useState<boolean>(false)
+
+const onDeleteClick = (produto: Produto) => {
+    if(deletando){
+        onDelete(produto)
+        setDeletando(false)
+    }else{
+        setDeletando(true)
+    }
+}
+
+const cancelarDelete = () => setDeletando(false)
+
     return(
         <tr>
             <td>{ produto.id }</td>
@@ -39,8 +69,19 @@ const ProdutoRow: React.FC<ProdutoRowProps> = ({
             <td>{ produto.nome }</td>
             <td>{ produto.preco }</td>
             <td>
-                <button className='button is-success'> Editar</button>
-                <button className='button is-danger'> Deletar</button>
+                { !deletando && <button onClick={e => onEdit(produto)} 
+                className='button is-success is-rounder is-small'>Editar</button>
+                }
+                                
+                <button onClick={e => onDelete(produto)} 
+                className='button is-danger is-rounder is-small'>{
+                deletando ? "Confirma ?" : "Deletar"
+                }</button>
+
+                { deletando && <button onClick={cancelarDelete} 
+                className='button is-rounder is-small'>Cancelar</button>
+
+                }
             </td>
         </tr>
     )
